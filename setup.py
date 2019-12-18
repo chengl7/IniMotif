9a9efc1aa6ff435ee920a3270b82b336b078b54c
Anna Price <36504362+annacprice@users.noreply.github.com>
2019-12-18 13:00:19 +0000

    initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name = "IniMotif",
+    version = "0.1.0",
+    python_requires='>=3.0',
+    install_requires = [
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'biopython',
+        'yattag',
+        'adjustText',
+        'seaborn',
+        'requests',
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
