acadc3adcb88ad433cd055ddb771cd135111c25c
Anna Price <36504362+annacprice@users.noreply.github.com>
2019-12-20 20:38:19 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,6 @@ from distutils.core import setup
 setup(
       name = "IniMotif",
       version = "0.1.0",
-      packages = setuptools.find_packages(),
-      scripts = [
-                 'inimotif_gui.py',
-                 'dna_logo.py',
-                 'inimotif_core.py',
-                 'inimotif_main.py',
-                 'inimotif_util.py',
-                 'windows.py',
-                 'chipWinExtract.py',
-                 'enaFastqFetch.py',
-                 'xmlparser.py',
-                 'reportwriter.py',
-                 ],
       python_requires='>=3.0',
       install_requires = [
                           'numpy',
@@ -31,6 +18,4 @@ setup(
                      "Programming Language :: Python :: 3",
                      "Operating System :: OS Independent",
                      ],
-      data_files=[('GUIgraphics', ['GUIgraphics/logo.png', 'GUIgraphics/masker.png'])],
-      include_package_data=True,
       )
